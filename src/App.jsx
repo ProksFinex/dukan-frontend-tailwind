0362afaea17b0ed9f01503282aaf6aafdc35a1f1
ProksFinex <prabhatjnu2@gmail.com>
2025-05-02 19:05:23 +0530

    working

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <div style={{backgroundColor: "red"}}>red</div>
+        <div style={{backgroundColor: "blueviolet"}}>blue</div>
+        <div style={{backgroundColor: "greenyellow"}}>green</div>
+      </div>
+    </>
+  )
+}
+
+export default App
