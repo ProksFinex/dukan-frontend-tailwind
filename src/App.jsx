343947b440d3cf5ac0452f3ca60f7273d2cdd712
ProksFinex <prabhatjnu2@gmail.com>
2025-05-13 00:51:24 +0530

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { RevenueCard } from "./Components/RevenueCard"
+
 function App() {
 
 
   return (
     <>
-      <div className="flex justify-between">
-        <div style={{backgroundColor: "red"}}>red</div>
-        <div style={{backgroundColor: "blueviolet"}}>blue</div>
-        <div style={{backgroundColor: "greenyellow"}}>green</div>
-      </div>
+    <div className="grid grid-cols-3">
+      <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={"13"}/>
+    </div>
+      
     </>
   )
 }
